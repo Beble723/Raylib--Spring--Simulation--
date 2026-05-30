551a0cc48654c534c856f2b98d2fdb162e2b9ff8
BeBle723 <waldowskikacper0@gmail.com>
2026-05-30 23:43:50 +0100

    added varibless

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,24 @@ int main(){
 
 constexpr int SCREEN_W= 900;
 constexpr int SCREEN_H= 600;
+constexpr int SPRING_TURNS=40;
+constexpr int SPRING_POS_X=SCREEN_W;
+constexpr int SPRING_POS_Y=SCREEN_H/2;
+
+struct main
+{
+    sliders x;
+    sliders y
+    Vector2l
+};
 
 
+
+bool isPaused =false;
+
+if(KEY_R){
+    isPaused = true;
+} 
 InitWindow(SCREEN_W,SCREEN_H,"Spring:Simulation");
 
 
@@ -24,8 +40,11 @@ BeginDrawing();
 
 
 
+DrawText("Space = pause | R= Reset",SCREEN_W-20,SCREEN_H-20,RAYWHITE);
+if(isPaused){
 
-
+    DrawText("Paused",SCREEN_W-20,SCREEN_H-20,RAYWHITE);
+}
 
 EndDrawing();
 }
